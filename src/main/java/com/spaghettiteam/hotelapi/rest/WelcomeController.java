9b6a990dafe848feea38dea1f8505102fc90853f
Cezary Krawczyk <cezary.krawczyk@poczta.pl>
2018-11-29 15:44:23 +0100

    added WelcomeController

---
+++ src/main/java/com/spaghettiteam/hotelapi/rest/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.spaghettiteam.hotelapi.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/api")
+    public String welcome() {
+        return "Hello world!";
+    }
+}
