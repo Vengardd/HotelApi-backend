168aa11023bb9f58ee186a49d73aa5d9ab70493a
Krystian Lukasiak <kryslukasiak@gmail.com>
2018-11-29 15:25:02 +0100

    initialize project

---
+++ src/main/java/com/spaghettiteam/hotelapi/HotelapiApplication.java
@@ -0,0 +1,12 @@
+package com.spaghettiteam.hotelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelapiApplication.class, args);
+	}
+}
