4dcb51a66b7653584492c06cd01f51beaf96e203
Cezary Krawczyk <cezary.krawczyk@poczta.pl>
2018-12-05 15:10:51 +0100

    added @Repository adnotation to UserRepository and RoomRepository

--- src/main/java/com/spaghettiteam/hotelapi/repository/RoomRepository.java
+++ src/main/java/com/spaghettiteam/hotelapi/repository/RoomRepository.java
@@ -2,6 +2,8 @@ package com.spaghettiteam.hotelapi.repository;
 
 import com.spaghettiteam.hotelapi.model.Room;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoomRepository extends CrudRepository<Room, Long> {
 }
--- src/main/java/com/spaghettiteam/hotelapi/repository/UserRepository.java
+++ src/main/java/com/spaghettiteam/hotelapi/repository/UserRepository.java
@@ -2,7 +2,8 @@ package com.spaghettiteam.hotelapi.repository;
 
 import com.spaghettiteam.hotelapi.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-
 }
