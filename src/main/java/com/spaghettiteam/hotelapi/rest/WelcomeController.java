9d2119ccb1f8e2a72206cf948a93bd9b0fa02b63
Cezary Krawczyk <cezary.krawczyk@poczta.pl>
2019-01-03 17:52:05 +0100

    still trying changing cors to work1

--- src/main/java/com/spaghettiteam/hotelapi/rest/WelcomeController.java
+++ src/main/java/com/spaghettiteam/hotelapi/rest/WelcomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
 
-    @CrossOrigin(origins = "*")
+//    @CrossOrigin(origins = "*")
     @GetMapping("/api")
     public String welcome() {
         return "Hello world!";
