446b814b3ff38a8e1655078bc0437134d69aaebc
Cezary Krawczyk <cezary.krawczyk@poczta.pl>
2019-01-03 17:10:08 +0100

    added CrossOrigin to welcome method

--- src/main/java/com/spaghettiteam/hotelapi/rest/WelcomeController.java
+++ src/main/java/com/spaghettiteam/hotelapi/rest/WelcomeController.java
@@ -1,11 +1,14 @@
 package com.spaghettiteam.hotelapi.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/api")
     public String welcome() {
         return "Hello world!";
