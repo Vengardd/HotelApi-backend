24f0f0265add813a7b3f0cb7b145c8af2e4ff2ab
Cezary Krawczyk <cezary.krawczyk@poczta.pl>
2019-01-04 22:21:18 +0100

    added CrossOrigin adnotation

--- src/main/java/com/spaghettiteam/hotelapi/rest/ReservationController.java
+++ src/main/java/com/spaghettiteam/hotelapi/rest/ReservationController.java
@@ -7,13 +7,11 @@ import com.spaghettiteam.hotelapi.model.Reservation;
 import com.spaghettiteam.hotelapi.model.Room;
 import com.spaghettiteam.hotelapi.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class ReservationController {
 
--- src/main/java/com/spaghettiteam/hotelapi/rest/WelcomeController.java
+++ src/main/java/com/spaghettiteam/hotelapi/rest/WelcomeController.java
@@ -1,13 +1,14 @@
 package com.spaghettiteam.hotelapi.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class WelcomeController {
 
 
-    //    @CrossOrigin(origins = "*")
     @GetMapping("/api")
     public String welcome() {
         return "Hello world!";
