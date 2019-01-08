9598c99655a9794bfd1ec4001306b80f93192a1f
Cezary Krawczyk <cezary.krawczyk@poczta.pl>
2019-01-08 16:21:01 +0100

    before changing properties to front

--- src/main/java/com/spaghettiteam/hotelapi/config/SecurityConfig.java
+++ src/main/java/com/spaghettiteam/hotelapi/config/SecurityConfig.java
@@ -43,28 +43,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return source;
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.cors();
-//        JwtWebSecurityConfigurer
-//                .forRS256(apiAudience, issuer)
-//                .configure(http)
-//                .authorizeRequests()
-//                .antMatchers(HttpMethod.GET, "/api").permitAll()
-//                .antMatchers(HttpMethod.GET, "/api/logged").authenticated()
-//                .antMatchers(HttpMethod.GET, "/api").permitAll()
-//                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
-//                .antMatchers(HttpMethod.POST, "/api/**").permitAll();
-//    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         JwtWebSecurityConfigurer
                 .forRS256(apiAudience, issuer)
                 .configure(http)
-                .cors().and().csrf().disable().authorizeRequests()
-                .anyRequest().permitAll();
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/logged").authenticated()
+                .antMatchers(HttpMethod.GET, "/api").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/**").permitAll();
     }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        JwtWebSecurityConfigurer
+//                .forRS256(apiAudience, issuer)
+//                .configure(http)
+//                .cors().and().csrf().disable().authorizeRequests()
+//                .anyRequest().permitAll();
+//    }
 
     @Bean
     public AuthenticationController authenticationController() throws UnsupportedEncodingException {
