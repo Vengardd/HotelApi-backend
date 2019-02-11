70b3cb12b61f474c3e3192a436057205659f8ac3
CzarekPiatek <36880660+CzarekPiatek@users.noreply.github.com>
2019-02-11 22:30:30 +0100

    cors updates

--- src/main/java/com/spaghettiteam/hotelapi/rest/WelcomeController.java
+++ src/main/java/com/spaghettiteam/hotelapi/rest/WelcomeController.java
@@ -1,21 +1,23 @@
 package com.spaghettiteam.hotelapi.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    @CrossOrigin(origins = "https://hotelapi-backend.herokuapp.com")
     @GetMapping("/api")
     public String welcome() {
         return "Hello world!";
     }
-
+    @CrossOrigin(origins = "https://hotelapi-backend.herokuapp.com")
     @GetMapping("/api/logged")
     public String welcomeForLogged() {
         return "Hello world for logged!";
     }
-
+    @CrossOrigin(origins = "https://hotelapi-backend.herokuapp.com")
     @GetMapping("/api/admin")
     public String welcomeForAdmin() {
         return "Hello admin!";
