a27233cec4d4a00e577a5951d74dcf1e13f86266
CzarekPiatek <36880660+CzarekPiatek@users.noreply.github.com>
2019-02-13 16:22:47 +0100

    deleted duplicated code from reservation service

--- src/main/java/com/spaghettiteam/hotelapi/service/ReservationService.java
+++ src/main/java/com/spaghettiteam/hotelapi/service/ReservationService.java
@@ -1,7 +1,6 @@
 package com.spaghettiteam.hotelapi.service;
 
 import com.spaghettiteam.hotelapi.dto.ReservationDTO;
-import com.spaghettiteam.hotelapi.dto.TwoDatesAndTwoPriceSearch;
 import com.spaghettiteam.hotelapi.dto.TwoDatesSearch;
 import com.spaghettiteam.hotelapi.exception.ReservationNotFoundException;
 import com.spaghettiteam.hotelapi.exception.RoomNotFoundException;
@@ -12,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-import static java.time.temporal.ChronoUnit.DAYS;
 
 @Service
 public class ReservationService {
@@ -60,26 +57,5 @@ public class ReservationService {
                 .withRoom(roomService.findById(reservationDTO.getRoomId()))
                 .withStartDate(reservationDTO.getStartDate())
                 .build());
-    }
-
-    @Deprecated
-    public List<Reservation> findAvailableRoomsBetweenDates(TwoDatesSearch twoDatesSearch) {
-        twoDatesSearchValidator.validate(twoDatesSearch);
-        return reservationRepository.findAllAvaibleRoomsBetweendDates(twoDatesSearch.getStartDate(), twoDatesSearch.getEndDate());
-    }
-
-    @Deprecated
-    public List<Reservation> findAvailableRoomsBetweenDatesAndPrices(TwoDatesAndTwoPriceSearch twoDatesAndTwoPriceSearch) {
-//        twoDatesAndTwoPriceSearchValidator.validate(twoDatesAndTwoPriceSearch);
-        long days = countDaysFromTwoDatesSearch(twoDatesAndTwoPriceSearch.getTwoDates());
-        long lowestPriceMuliplied = twoDatesAndTwoPriceSearch.getTwoPrices().getLowestPrice() * days;
-        long highestPriceMultiplied = twoDatesAndTwoPriceSearch.getTwoPrices().getHighestPrice() * days;
-        return reservationRepository.findAllAvaibleRoomsBetweendDatesAndWithinPrice(
-                twoDatesAndTwoPriceSearch.getTwoDates().getStartDate(), twoDatesAndTwoPriceSearch.getTwoDates().getEndDate(),
-                lowestPriceMuliplied, highestPriceMultiplied);
-    }
-
-    private long countDaysFromTwoDatesSearch(TwoDatesSearch twoDatesSearch) {
-        return DAYS.between(twoDatesSearch.getStartDate(), twoDatesSearch.getStartDate());
     }
 }
