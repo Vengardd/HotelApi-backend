b361642cd4562fbc25d291bc8621ab7309a59e61
Cezary Krawczyk <cezary.krawczyk@poczta.pl>
2019-02-13 15:19:04 +0100

    fix

--- src/main/java/com/spaghettiteam/hotelapi/service/RoomService.java
+++ src/main/java/com/spaghettiteam/hotelapi/service/RoomService.java
@@ -1,15 +1,12 @@
 package com.spaghettiteam.hotelapi.service;
 
 import com.spaghettiteam.hotelapi.dto.RoomDTO;
-import com.spaghettiteam.hotelapi.dto.TwoDatesSearch;
 import com.spaghettiteam.hotelapi.exception.RoomAlreadyExistException;
 import com.spaghettiteam.hotelapi.exception.RoomNotFoundException;
 import com.spaghettiteam.hotelapi.model.Room;
 import com.spaghettiteam.hotelapi.repository.room.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import static java.time.temporal.ChronoUnit.DAYS;
 
 @Service
 public class RoomService {
@@ -33,10 +30,5 @@ public class RoomService {
             return roomRepository.save(new Room(room.getRoomNumber()));
         }
         throw new RoomAlreadyExistException();
-    }
-
-
-    private long countDaysFromTwoDatesSearch(TwoDatesSearch twoDatesSearch) {
-        return DAYS.between(twoDatesSearch.getStartDate(), twoDatesSearch.getStartDate());
     }
 }
--- src/main/java/com/spaghettiteam/hotelapi/service/SearchService.java
+++ src/main/java/com/spaghettiteam/hotelapi/service/SearchService.java
@@ -6,11 +6,16 @@ import com.spaghettiteam.hotelapi.dto.TwoDatesSearch;
 import com.spaghettiteam.hotelapi.exception.RoomNotFoundException;
 import com.spaghettiteam.hotelapi.model.Reservation;
 import com.spaghettiteam.hotelapi.model.Room;
+import com.spaghettiteam.hotelapi.repository.reservation.ReservationRepository;
 import com.spaghettiteam.hotelapi.repository.room.RoomRepository;
+import com.spaghettiteam.hotelapi.validation.TwoDatesSearchValidator;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 @Service
 public class SearchService {
@@ -18,16 +23,22 @@ public class SearchService {
     @Autowired
     private RoomRepository roomRepository;
 
+    @Autowired
+    private TwoDatesSearchValidator twoDatesSearchValidator;
+    @Autowired
+    private ReservationRepository reservationRepository;
+
     public List<Room> findRooms(SearchDTO search) {
-        return roomRepository.findAvailableRooms(search.setLowestPrice(), search.getHighestPrice(), search.getStartDate(), search.getEndDate());
+        return roomRepository.findAvailableRooms(search.getLowestPrice(), search.getHighestPrice(), search.getStartDate(), search.getEndDate());
     }
 
+    @Deprecated
     public List<Reservation> findAvailableRoomsBetweenDates(TwoDatesSearch twoDatesSearch) {
         twoDatesSearchValidator.validate(twoDatesSearch);
         return reservationRepository.findAllAvaibleRoomsBetweendDates(twoDatesSearch.getStartDate(), twoDatesSearch.getEndDate());
     }
 
-
+    @Deprecated
     public List<Reservation> findAvailableRoomsBetweenDatesAndPrices(TwoDatesAndTwoPriceSearch twoDatesAndTwoPriceSearch) {
 //        twoDatesAndTwoPriceSearchValidator.validate(twoDatesAndTwoPriceSearch);
         long days = countDaysFromTwoDatesSearch(twoDatesAndTwoPriceSearch.getTwoDates());
@@ -37,11 +48,15 @@ public class SearchService {
                 twoDatesAndTwoPriceSearch.getTwoDates().getStartDate(), twoDatesAndTwoPriceSearch.getTwoDates().getEndDate(),
                 lowestPriceMuliplied, highestPriceMultiplied);
     }
-
+    @Deprecated
     public List<Room> findRoomsWithinPrice(long startPrice, long endPrice) {
         List<Room> rooms = roomRepository.findRoomsWithinPrice(startPrice, endPrice);
         if (rooms != null)
             return rooms;
         throw new RoomNotFoundException();
+    }
+
+    private long countDaysFromTwoDatesSearch(TwoDatesSearch twoDatesSearch) {
+        return DAYS.between(twoDatesSearch.getStartDate(), twoDatesSearch.getStartDate());
     }
 }
