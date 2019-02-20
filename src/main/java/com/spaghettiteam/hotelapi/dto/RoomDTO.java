b799fac91826792bd3adbcabdabf6234ee64d9a6
Cezary Krawczyk <cezary.krawczyk@poczta.pl>
2019-02-20 16:40:27 +0100

    added test rooms for front

--- src/main/java/com/spaghettiteam/hotelapi/dto/RoomDTO.java
+++ src/main/java/com/spaghettiteam/hotelapi/dto/RoomDTO.java
@@ -2,31 +2,94 @@ package com.spaghettiteam.hotelapi.dto;
 
 public class RoomDTO {
 
-    private String roomNumber;
-    private long pricePerDay;
+    private long roomId;
+    private double pricePerDay;
+    private String photoLink;
+    private float rating;
 
     public RoomDTO() {
     }
 
-    public RoomDTO(String roomNumber, long pricePerDay) {
-
-        this.roomNumber = roomNumber;
+    public RoomDTO(long roomId, double pricePerDay, String photoLink, float rating) {
+        this.roomId = roomId;
         this.pricePerDay = pricePerDay;
+        this.photoLink = photoLink;
+        this.rating = rating;
     }
 
-    public String getRoomNumber() {
-        return roomNumber;
+    public long getRoomId() {
+        return roomId;
     }
 
-    public void setRoomNumber(String roomNumber) {
-        this.roomNumber = roomNumber;
+    public void setRoomId(long roomId) {
+        this.roomId = roomId;
     }
 
-    public long getPricePerDay() {
+    public double getPricePerDay() {
         return pricePerDay;
     }
 
-    public void setPricePerDay(long pricePerDay) {
+    public void setPricePerDay(double pricePerDay) {
         this.pricePerDay = pricePerDay;
+    }
+
+    public String getPhotoLink() {
+        return photoLink;
+    }
+
+    public void setPhotoLink(String photoLink) {
+        this.photoLink = photoLink;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+
+    public static final class RoomDTOBuilder {
+        private long roomId;
+        private double pricePerDay;
+        private String photoLink;
+        private float rating;
+
+        private RoomDTOBuilder() {
+        }
+
+        public static RoomDTOBuilder aRoomDTO() {
+            return new RoomDTOBuilder();
+        }
+
+        public RoomDTOBuilder withRoomId(long roomId) {
+            this.roomId = roomId;
+            return this;
+        }
+
+        public RoomDTOBuilder withPricePerDay(double pricePerDay) {
+            this.pricePerDay = pricePerDay;
+            return this;
+        }
+
+        public RoomDTOBuilder withPhotoLink(String photoLink) {
+            this.photoLink = photoLink;
+            return this;
+        }
+
+        public RoomDTOBuilder withRating(float rating) {
+            this.rating = rating;
+            return this;
+        }
+
+        public RoomDTO build() {
+            RoomDTO roomDTO = new RoomDTO();
+            roomDTO.setRoomId(roomId);
+            roomDTO.setPricePerDay(pricePerDay);
+            roomDTO.setPhotoLink(photoLink);
+            roomDTO.setRating(rating);
+            return roomDTO;
+        }
     }
 }
--- src/main/java/com/spaghettiteam/hotelapi/rest/RoomController.java
+++ src/main/java/com/spaghettiteam/hotelapi/rest/RoomController.java
@@ -15,6 +15,33 @@ public class RoomController {
     @Autowired
     private RoomService roomService;
 
+    @GetMapping("/api/roomdto/test1")
+    public RoomDTO testRoomDTO() {
+        return RoomDTO.RoomDTOBuilder.aRoomDTO()
+                .withRoomId(1)
+                .withRating(3)
+                .withPricePerDay(70)
+                .withPhotoLink("http://static.e-aranzacje.pl/e-projektanci/realizations/4340/big_047d7ba7b4f71ba9bb2d758d9e142657cd9f1187.jpg")
+                .build();
+    }
+
+    @GetMapping("/api/room/test1")
+    public Room testRoom() {
+        return Room.RoomBuilder.aRoom()
+                .withId(1)
+                .withCapacity(2)
+                .withDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed odio a sapien vehicula eleifend at vel lacus. Morbi at turpis eu enim interdum ornare. Aliquam malesuada lacinia mi in accumsan. Proin eget enim elit. Phasellus tincidunt vehicula aliquet. Donec eu consectetur neque. Mauris at mollis eros. Nulla laoreet commodo ante. Integer elementum dignissim risus, in pulvinar risus aliquam quis. Donec volutpat at nisi sit amet tincidunt. Aenean eros risus, dignissim quis placerat et, dapibus vehicula erat. Vivamus non mollis mauris.\n" +
+                        "\n" +
+                        "Nullam sed dui feugiat mi auctor consequat. Vestibulum fringilla diam ex, quis auctor velit tincidunt suscipit. Fusce id dignissim nulla. Phasellus consequat bibendum mi a commodo. Aenean vulputate dolor id semper semper. Phasellus blandit, erat non ullamcorper rutrum, risus orci tincidunt magna, quis faucibus nulla dui id lectus. Suspendisse potenti. Phasellus mauris lacus, lobortis sit amet tellus quis, congue bibendum velit. Sed tristique, nisl quis auctor dictum, eros est scelerisque nisl, eu tincidunt libero tellus eget lectus. Vestibulum maximus sapien arcu, vel commodo quam eleifend at. Donec maximus augue velit, non auctor purus blandit id. Fusce neque erat, venenatis ac felis sodales, imperdiet aliquet risus. Nam vel nunc vitae ex mattis cursus. Cras vel finibus libero.\n" +
+                        "\n")
+                .withPetFriendly(true)
+                .withPhotoLink("http://static.e-aranzacje.pl/e-projektanci/realizations/4340/big_047d7ba7b4f71ba9bb2d758d9e142657cd9f1187.jpg")
+                .withPricePerDay(70)
+                .withRating(3)
+                .withRoomNumber("213a")
+                .build();
+    }
+
     @GetMapping("/api/room/number/{roomNumber}")
     public Room findByRoomNumber(@PathVariable("roomNumber") String roomNumber) {
         return roomService.findByRoomNumber(roomNumber);
